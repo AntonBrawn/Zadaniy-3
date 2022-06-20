d7601b09f653734cb755fffe5ab9c3a010e4ef52
Anton Abramov <ceasar0713@gmail.com>
2022-06-20 06:56:17 +0300

    Сделали домашнюю работу к 3 семинару

---
+++ hm1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+using System;
+using static System.Console;
+Clear();
+
+WriteLine("Введите 5 значное число");
+int number = Convert.ToInt32(ReadLine());
+
+if(number<10000 || number>99999)
+{
+    WriteLine($"{number} Введеное вами число не пятизначное, повторите попытку");
+}
+else
+{
+        int i = 0;
+        int k=10000;
+        int convNumber = 0;
+        int auxNumber = number;
+        while(i<5)
+        {
+            convNumber = convNumber + (auxNumber%10)*k;
+            auxNumber/=10;
+            k/=10;
+            i++;
+        }
+        WriteLine(convNumber==number? $"{number} это палидром" : $"{number} это не палидром");
+}
---
+++ hm2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+using System;
+using static System.Console;
+Clear();
+
+WriteLine("Введите  x1");
+int x1 = Convert.ToInt32(ReadLine());
+WriteLine("Введите y1");
+int y1 = Convert.ToInt32(ReadLine());
+WriteLine("Введите z1");
+int z1 = Convert.ToInt32(ReadLine());
+WriteLine("Введите x2");
+int x2 = Convert.ToInt32(ReadLine());
+WriteLine("Введите y2");
+int y2 = Convert.ToInt32(ReadLine());
+WriteLine("Введите z2");
+int z2 = Convert.ToInt32(ReadLine());
+
+double x= Math.Pow(x1-x2,2);
+double y= Math.Pow(y1-y2,2);
+double z= Math.Pow(z1-z2,2);
+double distance= Math.Sqrt(x+y+z);
+
+WriteLine($"Расстояние равно: {distance:f2}");
